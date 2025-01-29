b4915f97a03c11f850975ca0b78ed31715b32dbf
Joseph Izundu <gbimes@yahoo.com>
2025-01-29 17:03:50 +0800

    inclusion of DeepSeek Model in list of models

--- 1_chat_models/1_chat_model_basic.py
+++ 1_chat_models/1_chat_model_basic.py
@@ -4,16 +4,32 @@
 
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
+from langchain_deepseek import ChatDeepSeek  # include the deepseek api
 
 # Load environment variables from .env
 load_dotenv()
 
 # Create a ChatOpenAI model
-model = ChatOpenAI(model="gpt-4o-mini")
+# model = ChatOpenAI(model="gpt-4o-mini")
 
-# Invoke the model with a message
-result = model.invoke("What is 81 divided by 9?")
+#create a ChatDeepSeek model 
+#model_dps = ChatDeepSeek(model="deepseek-chat")
+model_dps = ChatDeepSeek(model="deepseek-reasoner")
+
+# Invoke the chatopenai model with a message
+# result = model.invoke("What is 81 divided by 9?")
+# print("Full result:")
+# print(result)
+# print("Content only:")
+# print(result.content)
+
+
+
+# Invoke the ChatDeepSeek model with a message
+result_dps = model_dps.invoke("What is 81 divided by 9?")
 print("Full result:")
-print(result)
+print(result_dps)
 print("Content only:")
-print(result.content)
+print(result_dps.content)
+
+
